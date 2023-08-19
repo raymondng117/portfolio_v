c7ec9c828da6afc9c8148be5e27b0fd2e7eb6b7f
Nywiz <ngyuwing117@gmail.com>
2023-08-19 15:25:00 -0400

    update

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,12 @@ import useFetch from './useFetch';
 
 
 const Home = () => {
-  //npx json-server --watch data/db.json --port 8000
+  //npm install npm-run-all --save-dev
+  //npm install json-server
+  //add lines below to the scripts in .json
+  //"start": "npm-run-all -p start:json-server start:react ",
+  //"start:react": "react-scripts start",
+  //"start:json-server": "npx json-server --watch data/db.json --port 8000",
   let url = 'http://localhost:8000/blogs'
   // ** assign a name to data (blogs)
   const {data:blogs, isPending, error} = useFetch(url)
