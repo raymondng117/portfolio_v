f06b13dbab2741cdc67159e6259c66289f94f589
Nywiz <ngyuwing117@gmail.com>
2023-08-20 01:27:05 -0400

    update

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,4 @@
-// import BlogList from './BlogList';
-// import useFetch from './useFetch';
-
+import React from 'react';
 
 const Home = () => {
   return (
@@ -8,23 +6,21 @@ const Home = () => {
       <article id="top" className="wrapper style1">
         <div className="container">
           <div className="row">
-
-            <div className="col-4 col-5-large col-12-medium ">
-           <img src="https://i.natgeofe.com/k/41de528e-c521-4a29-ade6-d385d9a4e5f7/sea-otter-closeup_16x9.jpg" className="rounded-circle img-fluid circle-img" alt="" />
+            <div className="col-md-4 col-12">
+              <img src="https://i.natgeofe.com/k/41de528e-c521-4a29-ade6-d385d9a4e5f7/sea-otter-closeup_16x9.jpg" className="rounded-circle img-fluid" alt="" />
             </div>
 
-             <div className="col-7 col-7-large col-12-medium" style={{ marginLeft: '20px' }}>
+            <div className="col-md-7 col-12" style={{ marginLeft: '60px' }}>
               <header>
                 <h1>Hi. I'm <strong>Raymond Ng</strong>.</h1>
               </header>
-              <p className="p-intro">And this is <strong>my portfolio website</strong> which presents my web development projects, demonstrating my skills and expertise in this field. You're welcome to explore these examples of my work, which reflect my commitment to creating polished digital solutions within the world of web development. </p>
-              <a href="#work" className="button large scrolly">Learn about what I do</a>
+              <p className="p-intro">And this is <strong>my portfolio website</strong>, showcasing my web development projects to demonstrate my skills and expertise in this field. You're welcome to explore these examples of my work, which reflect my commitment to creating polished digital solutions within the world of web development.</p>
+              <a href="#work" className="btn btn-primary mt-3">Learn about what I do</a>
             </div>
           </div>
         </div>
       </article>
     </div>
-
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,22 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-     return (
+    return (
         <nav className="navbar container">
-            <div className="d-flex justify-content-between align-items-center">
+            <div className="row justify-content-between align-items-center">
+                <div className="col-5">
+                    <h1 className="mr-auto">This is Raymond</h1>
+                </div>
 
-                <h1>This is Raymond</h1>
-                <ul className="list-unstyled m-0 d-flex">
-                    <li className="ml-3"><Link to="/" className="btn btn-secondary">About me</Link></li>
-                    <li className="ml-3"><Link to="/resume" className="btn btn-secondary">Resume</Link></li>
-                    <li className="ml-3"><Link to="/projects" className="btn btn-secondary">Projects</Link></li>
-                    <li className="ml-3"><Link to="/contact" className="btn btn-secondary">Contact</Link></li>
-                </ul>
+                <div className="col-7 justify-content-end">
+                    <ul className="list-unstyled m-0 d-flex links">
+                        <li className="ml-3"><Link to="/" className="btn btn-secondary">About me</Link></li>
+                        <li className="ml-3"><Link to="/resume" className="btn btn-secondary">Resume</Link></li>
+                        <li className="ml-3"><Link to="/projects" className="btn btn-secondary">Projects</Link></li>
+                        <li className="ml-3"><Link to="/contact" className="btn btn-secondary">Contact</Link></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
