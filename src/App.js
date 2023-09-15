ca5b0d7d78d5d79178a409d7c87e5013897509a8
Nywiz <ngyuwing117@gmail.com>
2023-09-14 22:41:28 -0400

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes} from 'react-router-dom';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
-import Skills from './Resume';
+import Resume from './Resume';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element = {<Home />}> </Route>
             <Route path='/home' element = {<Home />}> </Route>
-            <Route path='/resume' element={<Skills />}></Route>
+            <Route path='/resume' element={<Resume />}></Route>
             <Route path='/about' element={<About />}></Route>
             <Route path='/projects' element={<Projects />}></Route>
             <Route path='/contact' element={<Contact />}></Route>
