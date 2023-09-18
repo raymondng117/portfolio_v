d98c72d5cd99ea25a93e821c362d6d53b720bfb3
Nywiz <ngyuwing117@gmail.com>
2023-09-17 21:29:56 -0400

    update

--- src/projects.js
+++ src/projects.js
@@ -10,28 +10,13 @@ const Projects = () => {
                 <div className="col-md-4 col-sm-6 mt-3 " >
                     <div className="card text-black border-4 m-2" >
                         <div className="card-image" >
-                            <img class="card-img-top " src="https://th.bing.com/th/id/OIG.2W_p46W042h7V0BFZeoa?pid=ImgGn" alt="Live chat" />
+                            <img class="card-img-top " src="https://th.bing.com/th/id/OIG.2W_p46W042h7V0BFZeoa?pid=ImgGn" alt="MicrosoftAIimage" />
                         </div>
 
                         <div class="card-body">
-                            <h5 class="card-title">Live Chat App</h5>
+                            <h5 class="card-title fw-bolder">ChatHub</h5>
                             <p class="card-text text-sm">This App makes use of mongoDB, react.js, express.js and socket.io to create a page where users under same router can have consistent live chat with each other.</p>
-                            <a href="https://github.com/raymondng117/liveChat" class="btn btn-primary">More details</a>
-                        </div>
-                    </div>
-                </div>
-
-                {/*IntrovertCopilot*/}
-                <div className="col-md-4 col-sm-6 mt-3" >
-                    <div className="card text-black border-4 m-2" >
-                        <div className="card-image" >
-                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.mbNvkEtlS.LFPB4C8Pvg?pid=ImgGn" alt="Live chat" />
-                        </div>
-
-                        <div class="card-body">
-                            <h5 class="card-title">IntrovertCopilot</h5>
-                            <p class="card-text text-sm">This app make use of ChatGPT API chat module, express.js and JS to create a website for introverts to gain inspiration on how to initiate conversations.</p>
-                            <a href="https://github.com/tiffahahahu7/IntrovertCopilot.git" class="btn btn-primary">More details</a>
+                            <a href="https://github.com/raymondng117/liveChat" class="btn btn-primary project-links">Full Codes</a>
                         </div>
                     </div>
                 </div>
@@ -40,30 +25,62 @@ const Projects = () => {
                 <div className="col-md-4 col-sm-6 mt-3" >
                     <div className="card text-black border-4 m-2" >
                         <div className="card-image" >
-                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.dCZAhKbJKQu3aHkQK1GT?pid=ImgGn" alt="Live chat" />
+                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.dCZAhKbJKQu3aHkQK1GT?pid=ImgGn" alt="MicrosoftAIimage" />
                         </div>
 
                         <div class="card-body">
-                            <h5 class="card-title">Calculator</h5>
-                            <p class="card-text text-sm">This calculation application employs typical React functionalities like useState and useEffect to handle complex chained calculations and support negative number computations.</p>
-                            <a href=" https://raymondng117.github.io/freeCodeCamp_calculator/" class="btn btn-primary me-2">Live demo</a>
-                            <a href="https://github.com/raymondng117/freeCodeCamp_calculator" class="btn btn-primary">More details</a>
+                            <h5 class="card-title fw-bolder">Calculator</h5>
+                            <p class="card-text text-sm">This application employs typical React functionalities like useState and useEffect to handle complex chained calculations and negative number computations.</p>
+                            <a href=" https://raymondng117.github.io/freeCodeCamp_calculator/" class="btn btn-primary me-2 project-links">Live demo</a>
+                            <a href="https://github.com/raymondng117/freeCodeCamp_calculator" class="btn btn-primary project-links">Full Codes</a>
                         </div>
                     </div>
                 </div>
+
+                {/*Random Quotes*/}
+                <div className="col-md-4 col-sm-6 mt-3" >
+                    <div className="card text-black border-4 m-2" >
+                        <div className="card-image" >
+                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.VZiHGrtrU2oNw2i7jO3G?pid=ImgGn" alt="MicrosoftAIimage" />
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title fw-bolder">API Random Quotes</h5>
+                            <p class="card-text text-sm">This app make use of GitHub API, react.js features such as useEffect and useState to randomly create single fetched quotes.</p>
+                            <a href=" https://raymondng117.github.io/randomQuotes/" class="btn btn-primary me-2 project-links">Live demo</a>
+                            <a href="https://github.com/raymondng117/randomQuotes" class="btn btn-primary project-links">Full Codes</a>
+                        </div>
+                    </div>
+                </div>
+
+                {/*IntrovertCopilot*/}
+                <div className="col-md-4 col-sm-6 mt-3" >
+                    <div className="card text-black border-4 m-2" >
+                        <div className="card-image" >
+                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.mbNvkEtlS.LFPB4C8Pvg?pid=ImgGn" alt="MicrosoftAIimage" />
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title fw-bolder">IntrovertCopilot</h5>
+                            <p class="card-text text-sm">This app uses ChatGPT API, jQuery and localStroage to create a website for introverts to learn conversations skills.</p>
+                            <a href="https://github.com/tiffahahahu7/IntrovertCopilot.git" class="btn btn-primary project-links">Full Codes</a>
+                        </div>
+                    </div>
+                </div>
+
 
 
                 {/* APOD search */}
                 <div className="col-md-4 col-sm-6 mt-3" >
                     <div className="card text-black border-4 m-2" >
                         <div className="card-image" >
-                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.xdLdg5rky0lbwHFInpkJ?pid=ImgGn" alt="Live chat" />
+                            <img class="card-img-top border-dark border-4 " src="https://th.bing.com/th/id/OIG.xdLdg5rky0lbwHFInpkJ?pid=ImgGn" alt="MicrosoftAIimage" />
                         </div>
 
                         <div class="card-body">
-                            <h5 class="card-title">APOD search</h5>
+                            <h5 class="card-title fw-bolder">APOD search</h5>
                             <p class="card-text text-sm">This App makes use of API, local storage and fetch method to create a page where allow users to fetch images from NASA and save them in local storage.</p>
-                            <a href="https://github.com/raymondng117/liveChat" class="btn btn-primary">More details</a>
+                            <a href="https://github.com/raymondng117/liveChat" class="btn btn-primary project-links">Full Codes</a>
                         </div>
                     </div>
                 </div>
@@ -83,9 +100,9 @@ const Projects = () => {
                             </div>
 
                             <div class="card-body">
-                                <h5 class="card-title">Wander World</h5>
+                                <h5 class="card-title fw-bolder">Wander World</h5>
                                 <p class="card-text text-sm">This project employs a design application called Figma to create a travel-agency-like webpage. </p>
-                                <a href="https://www.figma.com/file/ZPtFbXJngR0s4yCepppOOH/Part1%3AWireframes?type=design&node-id=737-1651&mode=design&t=sdt1ezf0vsGjbfHk-0" class="btn btn-primary">More details</a>
+                                <a href="https://www.figma.com/file/ZPtFbXJngR0s4yCepppOOH/Part1%3AWireframes?type=design&node-id=737-1651&mode=design&t=sdt1ezf0vsGjbfHk-0" class="btn btn-primary project-links">Full Codes</a>
                             </div>
                         </div>
                     </div>
@@ -94,13 +111,13 @@ const Projects = () => {
                     <div className="col-md-4 col-sm-6 mt-3" >
                         <div className="card text-black border-4 m-2" >
                             <div className="card-image" >
-                                <img class="card-img-top border-dark border-4 " src={process.env.PUBLIC_URL + '/images/design1.png'} alt="Live chat" />
+                                <img class="card-img-top border-dark border-4 " src={process.env.PUBLIC_URL + '/images/design1.png'} alt="Illustrator project" />
                             </div>
 
                             <div class="card-body">
-                                <h5 class="card-title">Slothrill</h5>
-                                <p class="card-text text-sm">This image is created through employment of Adobe illustrator using layers and various techniques.</p>
-                                <a href="https://github.com/raymondng117/liveChat" class="btn btn-primary">More details</a>
+                                <h5 class="card-title fw-bolder">Slothrill</h5>
+                                <p class="card-text text-sm">This image is created through Adobe illustrator using <strong>layers</strong>, <strong>image tracking</strong>, <strong>selcetion tool </strong>and various techniques.</p>
+                                <a href="https://drive.google.com/file/d/1quONhk-uH497rYbovkR1VTGRTmBy2OHV/view?usp=drive_link" class="btn btn-primary">Document source</a>
                             </div>
                         </div>
                     </div>
